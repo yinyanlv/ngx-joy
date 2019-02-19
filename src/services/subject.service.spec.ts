8349510ed3072c44909eddc3cc4906517f439279
bugong <1761869346@qq.com>
2019-02-19 21:13:04 +0800

    fix: fix subject service test

--- src/services/subject.service.spec.ts
+++ src/services/subject.service.spec.ts
@@ -2,31 +2,22 @@ import {SubjectService} from './subject.service';
 
 describe('SubjectService', () => {
   let subject: SubjectService;
-  let globalData;
-  const obj = {
-    callback: function () {
-    }
-  };
 
   beforeAll(() => {
     subject = new SubjectService();
-    subject.subscribe('test', obj.callback);
+
   });
 
   it('should create subject', () => {
     expect(subject).toBeTruthy();
   });
 
-  beforeEach((done) => {
-    spyOn(obj, 'callback').and.callFake((data) => {
-      globalData = data;
+  it('should publish action success', (done: () => void) => {
+    subject.subscribe('test', (data) => {
+      expect(data).toEqual('data for test');
       done();
     });
 
     subject.publish('test', 'data for test');
-  });
-
-  it('should publish action success', () => {
-    expect(globalData).toEqual('data for test');
   });
 });
