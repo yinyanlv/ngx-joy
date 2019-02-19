511b210003c99d84e002c3b5d668e239b7829466
bugong <1761869346@qq.com>
2019-02-19 23:40:54 +0800

    feat: add ng-packagr

---
+++ public_api.ts
@@ -0,0 +1,2 @@
+export * from './src/app/components/components.module';
+export * from './src/app/services/services.module';
---
+++ src/app/services/services.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {SubjectService} from './subject.service';
+
+@NgModule({
+  providers: [
+    SubjectService
+  ]
+})
+export class ServicesModule {
+}
