484aefd296010e67f71dfe3d8962c77ece5b1a72
bugong <1761869346@qq.com>
2019-03-03 22:44:10 +0800

    edit nav

--- src/components/nav/src/nav.module.ts
+++ src/components/nav/src/nav.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatSidenavModule, MatListModule, MatIconModule, MatFormFieldModule} from '@angular/material';
 import {NavComponent} from './nav.component';
 
 @NgModule({
   declarations: [NavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
   exports: [
     NavComponent
